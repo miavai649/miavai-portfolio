98bcde491406433444a2ff0a048da585e1f49e49
miavai649 <mahmudulhaquenoor@gmail.com>
2024-10-21 02:13:48 +0600

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { DatePicker } from 'antd'
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <DatePicker />
+    </>
+  )
+}
+
+export default App
